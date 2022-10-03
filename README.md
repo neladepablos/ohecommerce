3f12a40df93406d16e352838c46099e1b575e51a
Marianela Ruiz <marianelaruizdepablos@gmail.com>
2022-10-02 20:07:45 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+<!--  npx node-sass --watch -o src/styles/css/  src/styles/sass/styles.scss  -->
---
+++ src/js/index.js
@@ -0,0 +1,34 @@
+// Function to show and hide the popup
+function togglePopup() {
+    var width = $(window).width(); 
+
+    if (width >= 820) {
+        $(".content-hide").toggle();
+    }
+    else {    
+        alert("Redirigiendo a carrito de compra...")
+    }
+
+}
+
+// Click profile icons/class
+// $(".click-profile").on('click', function(event){
+//     alert("Redirigiendo al perfil del cliente...")
+// });
+
+
+/**  Click para el menu  **/
+ const showMenu = (toggleId, navId) => {
+    const toggle = document.getElementById(toggleId);
+     const nav = document.getElementById(navId);
+
+     if (toggle && nav) {
+         toggle.addEventListener('click', () => {
+             nav.classList.toggle('show')
+         })
+
+     }
+}
+
+showMenu('menu-hamb-toggle', 'hamburgerMenu');
+
